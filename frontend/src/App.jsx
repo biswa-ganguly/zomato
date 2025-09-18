346817ee4e6b2423b481da05963cefd6f8053a42
biswa-ganguly <biswaganguly16@gmail.com>
2025-09-18 15:11:30 +0530

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,13 +22,13 @@ function App() {
             <Route path="/register" element={<ChooseRegister />} />
             <Route path="/user/register" element={<UserRegister />} />
             <Route path="/user/login" element={<UserLogin />} />
-            <Route path="/user/profile" element={<UserProfile />} />
+            <Route path="/user/profile" element={<> <UserProfile /> <BottomNav/> </>} />
             <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
             <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
             <Route path="/" element={<><Home /><BottomNav /></>} />
             <Route path="/saved" element={<><Saved /><BottomNav /></>} />
             <Route path="/create-food" element={<CreateFood />} />
-            <Route path="/food-partner/:id" element={<Profile />} />
+            <Route path="/food-partner/:id" element={<> <Profile /><BottomNav /> </>} />
           </Routes>
         </div>
       </Router>
