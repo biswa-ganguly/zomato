e98c95312f24e1405f5a7bb74733eb494604e400
biswa-ganguly <biswaganguly16@gmail.com>
2025-09-18 12:03:15 +0530

    food partner made

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser');
 const authRoutes = require("./routes/auth.route")
 const foodRoutes = require('./routes/food.route');
+const foodPartnerRoutes = require('./routes/foodpartner.route')
 
 const app = express()
 app.use(cookieParser());
@@ -13,6 +14,8 @@ app.get("/", (req,res )=>{
 
 app.use("/api/auth", authRoutes)
 app.use("/api/food", foodRoutes)
+app.use('/api/food-partner', foodPartnerRoutes);
+
 
 module.exports = app
 
